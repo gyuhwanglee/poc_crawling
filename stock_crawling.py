1e76ef58a149c9693c726c58c25d8e67d3767e90
gyuhwanglee <79841624+gyuhwanglee@users.noreply.github.com>
2022-03-01 19:42:13 +0900

    주식 종목 3개 가격 가져오기

--- stock_crawling.py
+++ stock_crawling.py
@@ -1,13 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/item/main.nhn?code=005930"
-res = requests.get(url)
-html = res.text
-soup = BeautifulSoup(html, 'html.parser')
-sel = soup.find('div', "today").text
-sel = sel.replace(',', '')
-print(sel)
+arr_Stock = [
+    "005930",
+    "000660",
+    "035720"
+]
+# 삼성
+# 에스케이하이닉스
+# 카카오
+for sotck in arr_Stock:
+    url = f"https://finance.naver.com/item/main.nhn?code={sotck}"
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    sel = soup.find('p', "no_today").text
+    sel = sel.replace(',', '')
+    print(sel)
 
 
 # res = requests.get("https://m.stock.naver.com/worldstock/stock/SDGR.O/total")
