07252963307293c0ecba80e401ca4b60d237ef6d
gyuhwanglee <79841624+gyuhwanglee@users.noreply.github.com>
2022-03-02 12:50:05 +0900

    naver stock info

--- stock_crawling.py
+++ stock_crawling.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup
 arr_Stock = [
     "005930",
     "000660",
-    "035720"
+    "035720",
+    "054180",
+    "099220"
 ]
 # 삼성
 # 에스케이하이닉스
@@ -15,8 +17,17 @@ for sotck in arr_Stock:
     html = res.text
     soup = BeautifulSoup(html, 'html.parser')
     sel = soup.find('p', "no_today").text
-    sel = sel.replace(',', '')
-    print(sel)
+    # sel = sel.replace(',', '')
+    selname = soup.find(
+        'title').text
+    sliceName = selname.split(':')[0]
+    # relatedName = soup.find(
+    #     'div', 'rate_info')
+    # for i in relatedName:
+    #     print(i.string)
+    todayInfo = soup.find('div', {'class': 'today'}).text
+    # find 찾기는 (태그값 , {속성값})
+    print(sel, sliceName, todayInfo)
 
 
 # res = requests.get("https://m.stock.naver.com/worldstock/stock/SDGR.O/total")
